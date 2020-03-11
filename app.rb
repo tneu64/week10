161c0629e10e374724cf01a0a6f262f6e9709905
Benjamin Block <bjblock@gmail.com>
2020-03-11 13:59:57 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,136 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sinatra"
+require "sinatra/cookies"                                                             #
+require "sinatra/reloader" if development?                                            #
+require "sequel"                                                                      #
+require "logger"                                                                      #
+require "bcrypt"                                                                      #
+require "twilio-ruby"                                                                 #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                              #
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
+def view(template); erb template.to_sym; end                                          #
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
+before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
+after { puts; }                                                                       #
+#######################################################################################
+
+events_table = DB.from(:events)
+rsvps_table = DB.from(:rsvps)
+users_table = DB.from(:users)
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+# homepage and list of events (aka "index")
+get "/" do
+    puts "params: #{params}"
+
+    pp events_table.all.to_a
+    @events = events_table.all.to_a
+    view "events"
+end
+
+# event details (aka "show")
+get "/events/:id" do
+    puts "params: #{params}"
+
+    @users_table = users_table
+    @event = events_table.where(id: params[:id]).to_a[0]
+    pp @event
+    @rsvps = rsvps_table.where(event_id: @event[:id]).to_a
+    @going_count = rsvps_table.where(event_id: @event[:id], going: true).count
+    view "event"
+end
+
+# display the rsvp form (aka "new")
+get "/events/:id/rsvps/new" do
+    puts "params: #{params}"
+
+    @event = events_table.where(id: params[:id]).to_a[0]
+    view "new_rsvp"
+end
+
+# receive the submitted rsvp form (aka "create")
+post "/events/:id/rsvps/create" do
+    puts "params: #{params}"
+
+    # first find the event that rsvp'ing for
+    @event = events_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvps table with the rsvp form data
+    rsvps_table.insert(
+        event_id: @event[:id],
+        user_id: session["user_id"],
+        comments: params["comments"],
+        going: params["going"]
+    )
+    view "create_rsvp"
+end
+
+get "/rsvps/:id/edit" do
+    puts "params: #{params}"
+
+    view "edit_rsvp"
+end
+
+post "/rsvps/:id/update" do
+    puts "params: #{params}"
+
+    view "update_rsvp"
+end
+
+get "/rsvps/:id/destroy" do
+    puts "params: #{params}"
+
+    view "destroy_rsvp"
+end
+
+# display the signup form (aka "new")
+get "/users/new" do
+    view "new_user"
+end
+
+# receive the submitted signup form (aka "create")
+post "/users/create" do
+    puts "params: #{params}"
+
+    users_table.insert(
+        name: params["name"],
+        email: params["email"],
+        password: BCrypt::Password.create(params["password"])
+    )
+    view "create_user"
+end
+
+# display the login form (aka "new")
+get "/logins/new" do
+    view "new_login"
+end
+
+# receive the submitted login form (aka "create")
+post "/logins/create" do
+    puts "params: #{params}"
+
+    # step 1: user with the params["email"] ?
+    @user = users_table.where(email: params["email"]).to_a[0]
+    if @user
+        # step 2: if @user, does the encrypted password match?
+        if BCrypt::Password.new(@user[:password]) == params["password"]
+            # set encrypted cookie for logged in user
+            session["user_id"] = @user[:id]
+            view "create_login"
+        else
+            view "create_login_failed"
+        end
+    else
+        view "create_login_failed"
+    end
+end
+
+# logout user
+get "/logout" do
+    # remove encrypted cookie for logged out user
+    session["user_id"] = nil
+    view "logout"
+end
---
+++ createdb.rb
@@ -0,0 +1,44 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sequel"                                                                      #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                              #
+#######################################################################################
+
+# Database schema - this should reflect your domain model
+
+# New domain model - adds users
+DB.create_table! :events do
+  primary_key :id
+  String :title
+  String :description, text: true
+  String :date
+  String :location
+end
+DB.create_table! :rsvps do
+  primary_key :id
+  foreign_key :event_id
+  foreign_key :user_id
+  Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+end
+
+# Insert initial (seed) data
+events_table = DB.from(:events)
+
+events_table.insert(title: "Bacon Burger Taco Fest", 
+                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
+                    date: "June 21",
+                    location: "Kellogg Global Hub")
+
+events_table.insert(title: "Kaleapolooza", 
+                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
+                    date: "July 4",
+                    location: "Nowhere")
+
+puts "Success!"
